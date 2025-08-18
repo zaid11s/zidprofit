9649528239889b5eabcbb216044955a503bd402d
zaid11s <zaidsalah558@gmail.com>
2025-08-18 18:17:31 +0300

    Update service-worker.js
    Signed-off-by: zaid11s <zaidsalah558@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -19,3 +19,21 @@
     }
   ]
 }
+const CACHE = "zidprofit-cache-v2";
+const ASSETS = [
+  "./","./index.html","./style.css","./script.js",
+  "./manifest.json","./icon-192.png","./icon-512.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy));
+      return res;
+    }).catch(()=>r))
+  );
+});
